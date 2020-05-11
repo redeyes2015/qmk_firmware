73cc09a3449ab83f23802e1a840ffd9b313ed744
redeyes2015 <redeyes0215@gmail.com>
2020-05-11 15:08:28 +0800

    Replace RGBLIGHT_LAYERS with custom logic
    Turns out RGBLIGHT_LAYERS only supports 8 layers, so I have to write my
    code to drive it. Thankfully, it is not very complex.

--- keyboards/crkbd/keymaps/rein/rgb.c
+++ keyboards/crkbd/keymaps/rein/rgb.c
@@ -1,74 +1,56 @@
 #include QMK_KEYBOARD_H
 
-#ifdef RGBLIGHT_LAYERS
+#ifdef RGBLIGHT_ENABLE
 
-#define RGBLIGHT_THUMBS(color) RGBLIGHT_LAYER_SEGMENTS( \
-    {6, 1, color}, \
-    {13, 1, color}, \
-    {14, 1, color} \
-)
-
-const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_THUMBS(HSV_RED);
-const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_THUMBS(HSV_YELLOW);
-const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_THUMBS(HSV_ORANGE);
-const rgblight_segment_t PROGMEM my_layer4_layer[] = RGBLIGHT_THUMBS(HSV_PURPLE);
-
-const rgblight_segment_t PROGMEM my_shift_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {10, 3, HSV_WHITE}
-);
-const rgblight_segment_t PROGMEM my_ctrl_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {15, 3, HSV_WHITE}
-);
-const rgblight_segment_t PROGMEM my_alt_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {18, 3, HSV_WHITE}
-);
-const rgblight_segment_t PROGMEM my_gui_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {21, 3, HSV_WHITE}
-);
-
-enum my_rgblayout_index {
-    my_layer1_index = 0,
-    my_layer2_index,
-    my_layer3_index,
-    my_layer4_index,
-    my_shift_index,
-    my_ctrl_index,
-    my_alt_index,
-    my_gui_index
-};
-
-#define MY_LAYER_DEFINE(name) [name##_index] = name##_layer
-
-// Now define the array of layers. Later layers take precedence
-const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    MY_LAYER_DEFINE(my_layer1),
-    MY_LAYER_DEFINE(my_layer2),
-    MY_LAYER_DEFINE(my_layer3),
-    MY_LAYER_DEFINE(my_layer4),
-    MY_LAYER_DEFINE(my_shift),
-    MY_LAYER_DEFINE(my_ctrl),
-    MY_LAYER_DEFINE(my_alt),
-    MY_LAYER_DEFINE(my_gui)
-);
+#define HSV_BLACK 0,0,0
 
 void keyboard_post_init_user(void) {
-    // Enable the LED layers
-    rgblight_layers = my_rgb_layers;
-    rgblight_sethsv_noeeprom(0, 0, 0);
+    rgblight_sethsv_noeeprom(HSV_BLACK);
+}
+
+#define HSV_SET_THUMB(color) { \
+        rgblight_sethsv_at(color, 6); \
+        rgblight_sethsv_at(color, 13); \
+        rgblight_sethsv_at(color, 14); \
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(my_layer1_index, layer_state_cmp(state, 1));
-    rgblight_set_layer_state(my_layer2_index, layer_state_cmp(state, 2));
-    rgblight_set_layer_state(my_layer3_index, layer_state_cmp(state, 3));
-    rgblight_set_layer_state(my_layer4_index, layer_state_cmp(state, 4));
+    switch (get_highest_layer(state)) {
+        case 1:
+            HSV_SET_THUMB(HSV_RED);
+            break;
+        case 2:
+            HSV_SET_THUMB(HSV_YELLOW);
+            break;
+        case 3:
+            HSV_SET_THUMB(HSV_CYAN);
+            break;
+        case 4:
+            HSV_SET_THUMB(HSV_PURPLE);
+            break;
+        case 5:
+            HSV_SET_THUMB(HSV_BLUE);
+            break;
+        default:
+            HSV_SET_THUMB(HSV_BLACK);
+            break;
+    }
     return state;
 }
 
+static void set_mod_light (uint8_t mods, uint8_t mod_mask, uint8_t begin) {
+    if ((mods & mod_mask) != 0) {
+        rgblight_sethsv_range(HSV_BLUE, begin, begin + 3);
+    }
+    else {
+        rgblight_sethsv_range(HSV_BLACK, begin, begin + 3);
+    }
+}
+
 void oneshot_mods_changed_user(uint8_t mods) {
-    rgblight_set_layer_state(my_shift_index, (mods & MOD_MASK_SHIFT) != 0);
-    rgblight_set_layer_state(my_ctrl_index, (mods & MOD_MASK_CTRL) != 0);
-    rgblight_set_layer_state(my_alt_index, (mods & MOD_MASK_ALT) != 0);
-    rgblight_set_layer_state(my_gui_index, (mods & MOD_MASK_GUI) != 0);
+    set_mod_light(mods, MOD_MASK_SHIFT, 10);
+    set_mod_light(mods, MOD_MASK_CTRL, 15);
+    set_mod_light(mods, MOD_MASK_ALT, 18);
+    set_mod_light(mods, MOD_MASK_GUI, 21);
 }
 #endif
